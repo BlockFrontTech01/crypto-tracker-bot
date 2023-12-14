0b3c852151004e891c55d9b7728a4f13840fbb8e
Parking Swing <148919566+parkingswing2323@users.noreply.github.com>
2023-12-14 14:04:45 +0100

    Update cryptotracker-menu.js

--- components/cryptotracker-menu.js
+++ components/cryptotracker-menu.js
@@ -32,7 +32,7 @@ const CryptotrackerMenu = ({ onClose }) => {
   }, []);
 
   const onFrameButton2Click = useCallback(() => {
-    window.open("https://parkingswing.vercel.app");
+    window.open("https://serparking.vercel.app");
   }, []);
 
   return (
