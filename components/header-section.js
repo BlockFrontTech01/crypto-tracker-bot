6c52a391cf007621ecaf2ecbcb1ca781f41e0590
Jeff <148919566+jeff96655@users.noreply.github.com>
2023-12-28 16:22:05 +0100

    Update header-section.js

--- components/header-section.js
+++ components/header-section.js
@@ -7,7 +7,7 @@ const HeaderSection = () => {
     useState(false);
 
   const onFrameButtonClick = useCallback(() => {
-    window.open("https://jeffspace.vercel.app");
+    window.open("https://t.me/jeff96655");
   }, []);
 
   const onFrameButton1Click = useCallback(() => {
